73bda21d1becb03359e46e1005b92f97ffd324b5
Michel van der Hoeven <info@werkgever-gezocht.nl>
2023-09-26 18:42:47 +0200

    kleine typfout gewijzigd

--- components/Transition/Transition.tsx
+++ components/Transition/Transition.tsx
@@ -36,7 +36,6 @@ const Transition = () => {
         exit="exit"
         transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
       >
-        1
       </motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-yellow-200"
@@ -46,7 +45,6 @@ const Transition = () => {
         exit="exit"
         transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
       >
-        1
       </motion.div>
 
     </>
